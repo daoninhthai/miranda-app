bc869ff8c49f0fca6aa738d9c1f7037216744434
daoninhthai <thaimeo1131@gmail.com>
2025-03-30 11:00:11 +0700

    done Notification

--- code/Magetop/Notification/Controller/Adminhtml/SellerCustom.php
+++ code/Magetop/Notification/Controller/Adminhtml/SellerCustom.php
@@ -1,6 +1,6 @@
 <?php
 namespace Magetop\Notification\Controller\Adminhtml;
-class ProductsCustom extends \Magetop\Marketplace\Controller\Adminhtml\Products\MassStatus
+class SellerCustom extends \Magetop\Marketplace\Controller\Adminhtml\Sellers\MassStatus
 {
     protected function _massStatusAction()
     {
@@ -9,58 +9,38 @@ class ProductsCustom extends \Magetop\Marketplace\Controller\Adminhtml\Products\
         if (!is_array($ids)) {
             $ids = [$ids];
         }
-
         $model = $this->_getModel(false);
         $error = false;
-
         try {
-            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
             $status = $this->getRequest()->getParam('status');
             $statusFieldName = $this->_statusField;
-
             if (is_null($status)) {
                 throw new \Exception(__('Parameter "Status" missing in request data.'));
             }
-
             if (is_null($statusFieldName)) {
                 throw new \Exception(__('Status Field Name is not specified.'));
             }
-
-            foreach ($ids as $id) {
-                $sellerProductModel = $this->_objectManager->create($this->_modelClass)
+            foreach($ids as $id) {
+                $data = $this->_objectManager->create($this->_modelClass)
                     ->load($id)
                     ->setData($this->_statusField, $status)
                     ->save();
-
-                $product = $objectManager->create('\Magento\Catalog\Api\ProductRepositoryInterface')
-                    ->getById($sellerProductModel->getProductId());
-                $product->setStatus($status);
-                $objectManager->create('\Magento\Catalog\Api\ProductRepositoryInterface')->save($product);
-
-                $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
-                $connection = $resource->getConnection();
-                $tableName = $resource->getTableName('multivendor_product');
-                $sql = "SELECT * FROM " . $tableName . " WHERE product_id = " . $sellerProductModel->getProductId();
-                $result = $connection->fetchAll($sql);
-
-                if ($status == 1) {
-                    $pStatus = __('Approved');
-                    $notificationType = 1; // Type for approved products
-                } else {
-                    $pStatus = __('Unapproved');
-                    $notificationType = 2; // Type for unapproved products
+                if($status == 1){
+                    $sStatus = __('Approved');
+                    $notificationType = 1; // Type for approved seller
+                    $this->_objectManager->create('Magetop\Marketplace\Helper\EmailSeller')->sendApproveSellerEmail($data->getUserId());
+                }else{
+                    $sStatus = __('Disapproved');
+                    $notificationType = 2; // Type for unapproved seller
+                    $this->_objectManager->create('Magetop\Marketplace\Helper\EmailSeller')->sendUnapproveSellerEmail($data->getUserId());
                 }
-
-                // Send email
-                $this->_objectManager->create('Magetop\Marketplace\Helper\EmailSeller')
-                    ->sendProductSellerEmail($product->getName(), $result[0]['user_id'], $pStatus);
 
                 // Add notification record
                 $notifyModel = $this->_objectManager->create('Magetop\Notification\Model\Notification');
-                $notificationContent = __('Your product "%1" has been %2', $product->getName(), strtolower($pStatus));
+                $notificationContent = __('You have been %1 as a seller.', strtolower($sStatus));
 
                 $notificationData = [
-                    'seller_id' => $result[0]['user_id'], // Get seller ID from multivendor_product table
+                    'seller_id' => $data->getUserId(), // Get seller ID from multivendor_product table
                     'type' => $notificationType, // 1 for approved, 2 for unapproved
                     'content' => $notificationContent,
                     'status' => 0, // 0 for unread, 1 for read
@@ -69,12 +49,7 @@ class ProductsCustom extends \Magetop\Marketplace\Controller\Adminhtml\Products\
 
                 $notifyModel->setData($notificationData);
                 $notifyModel->save();
-//                $connection->insert(
-//                    $resource->getTableName('multivendor_notification'),
-//                    $notificationData
-//                );
             }
-
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
             $error = true;
             $this->messageManager->addError($e->getMessage());
@@ -82,13 +57,11 @@ class ProductsCustom extends \Magetop\Marketplace\Controller\Adminhtml\Products\
             $error = true;
             $this->messageManager->addException($e, __('We can\'t change status of '.strtolower($model->getOwnTitle()).' right now. '.$e->getMessage()));
         }
-
         if (!$error) {
             $this->messageManager->addSuccess(
                 __($model->getOwnTitle(count($ids) > 1).' status have been changed.')
             );
         }
-
         $this->_redirect('*/*');
     }
 }

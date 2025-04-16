ac8c2aa171a803a01d037c3a8de69a88677c9f76
daoninhthai <thaimeo1131@gmail.com>
2025-04-16 22:55:33 +0700

    fix: prevent division by zero in calculation

--- code/Magetop/ProductAttributeManagement/Controller/Adminhtml/ProductAttributeManagement/NewAction.php
+++ code/Magetop/ProductAttributeManagement/Controller/Adminhtml/ProductAttributeManagement/NewAction.php
@@ -4,4 +4,16 @@ namespace Magetop\ProductAttributeManagement\Controller\Adminhtml\ProductAttribu
 class NewAction extends \Magetop\ProductAttributeManagement\Controller\Adminhtml\ProductAttributeManagement
 {
 
+
+    /**
+     * Sanitizes a string for safe output.
+     *
+     * @param string $input
+     * @return string
+     */
+    protected function sanitizeOutput($input)
+    {
+        return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
+    }
+
 }
--- code/Magetop/SellerMembership/Controller/Adminhtml/SellerMembership/NewAction.php
+++ code/Magetop/SellerMembership/Controller/Adminhtml/SellerMembership/NewAction.php
@@ -4,4 +4,16 @@ namespace Magetop\SellerMembership\Controller\Adminhtml\SellerMembership;
 class NewAction extends \Magetop\SellerMembership\Controller\Adminhtml\SellerMembership
 {
 
+
+    /**
+     * Sanitizes a string for safe output.
+     *
+     * @param string $input
+     * @return string
+     */
+    protected function sanitizeOutput($input)
+    {
+        return htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8');
+    }
+
 }

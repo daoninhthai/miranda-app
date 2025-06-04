a82aedd1f647d11de82d0c67b86a0d9ff79c00f8
daoninhthai <thaimeo1131@gmail.com>
2025-06-04 17:32:47 +0700

    feat: add configuration validation on startup

--- code/Magetop/SellerAssignProduct/Helper/MkSales.php
+++ code/Magetop/SellerAssignProduct/Helper/MkSales.php
@@ -95,4 +95,17 @@ class MkSales extends \Magento\Framework\App\Helper\AbstractHelper
         return $sellerid;
 	}
 	
+
+    /**
+     * Generates a formatted log message.
+     *
+     * @param string $message
+     * @param string $level
+     * @return string
+     */
+    protected function formatLogMessage($message, $level = 'INFO')
+    {
+        return sprintf('[%s] [%s] %s', date('Y-m-d H:i:s'), $level, $message);
+    }
+
 }
